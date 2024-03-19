42af126b653719ab74410ed6cc77b67bc11b6861
若海 <wang@rehiy.com>
2024-03-19 09:52:25 +0800

    fix: site查询不返回空字段

--- api/module/site.php
+++ api/module/site.php
@@ -21,46 +21,46 @@ class Site
         }
 
         $list = [];
-        if (isset($data['domain_registrar'])) {
+        if (!empty($data['domain_registrar'])) {
             $list[] = '注册商：' . $data['domain_registrar'];
         }
-        if (isset($data['domain_expired'])) {
+        if (!empty($data['domain_expired'])) {
             $list[] = '是否过期：' . ($data['domain_expired'] ? '是' : '否');
         }
-        if (isset($data['domain_status'])) {
+        if (!empty($data['domain_status'])) {
             $list[] = '域名状态：' . implode('、', $data['domain_status']);
         }
-        if (isset($data['authoritative_dns'])) {
+        if (!empty($data['authoritative_dns'])) {
             $list[] = '权威DNS：' . implode('、', $data['authoritative_dns']);
         }
-        if (isset($data['authoritative_dns_resolve'])) {
+        if (!empty($data['authoritative_dns_resolve'])) {
             $list[] = '权威DNS解析：';
             foreach ($data['authoritative_dns_resolve'] as $item) {
                 $list[] = "    {$item['type']}/{$item['ttl']}：" . implode('、', $item['value']);
             }
         }
-        if (isset($data['recursive_dns_resolve'])) {
+        if (!empty($data['recursive_dns_resolve'])) {
             $list[] = '递归DNS解析：';
             foreach ($data['recursive_dns_resolve'] as $item) {
                 $list[] = "    {$item['type']}/{$item['ttl']}：" . implode('、', $item['value']);
             }
         }
-        if (isset($data['ttl'])) {
+        if (!empty($data['ttl'])) {
             $list[] = 'TTL：' . $data['ttl'];
         }
-        if (isset($data['icp_black'])) {
+        if (!empty($data['icp_black'])) {
             $list[] = 'ICP 黑名单：' . ($data['icp_black'] ? '是' : '否');
         }
-        if (isset($data['icp_number'])) {
+        if (!empty($data['icp_number'])) {
             $list[] = 'ICP 备案号：' . $data['icp_number'];
         }
-        if (isset($data['miit_black'])) {
+        if (!empty($data['miit_black'])) {
             $list[] = 'MIIT 黑名单：' . ($data['miit_black'] ? '是' : '否');
         }
-        if (isset($data['http_status'])) {
+        if (!empty($data['http_status'])) {
             $list[] = 'HTTP 状态：' . $data['http_status'];
         }
-        if (isset($data['ping_status'])) {
+        if (!empty($data['ping_status'])) {
             $list[] = 'PING 状态：' . $data['ping_status'];
         }
 
