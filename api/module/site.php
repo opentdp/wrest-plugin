4349aec9f723e093e0eb1dcb1db9a9dae4df00f5
若海 <wang@rehiy.com>
2024-03-19 10:31:22 +0800

    fix: 调整适合微信消息的格式

--- api/module/site.php
+++ api/module/site.php
@@ -24,44 +24,49 @@ class Site
         if (!empty($data['domain_registrar'])) {
             $list[] = '注册商：' . $data['domain_registrar'];
         }
-        if (!empty($data['domain_expired'])) {
+        if (isset($data['domain_expired'])) {
             $list[] = '是否过期：' . ($data['domain_expired'] ? '是' : '否');
         }
         if (!empty($data['domain_status'])) {
             $list[] = '域名状态：' . implode('、', $data['domain_status']);
         }
+        if (isset($data['icp_black'])) {
+            $list[] = 'ICP 黑名单：' . ($data['icp_black'] ? '是' : '否');
+        }
+        if (isset($data['icp_number'])) {
+            $list[] = 'ICP 备案号：' . ($data['icp_number'] ?: '未知');
+        }
+        if (isset($data['miit_black'])) {
+            $list[] = 'MIIT 黑名单：' . ($data['miit_black'] ? '是' : '否');
+        }
+        if (isset($data['http_status'])) {
+            $list[] = 'HTTP 状态：' . $data['http_status'];
+        }
+        if (isset($data['ping_status'])) {
+            $list[] = 'PING 状态：' . $data['ping_status'];
+        }
+        if (!empty($data['ttl'])) {
+            $list[] = 'TTL 生效时间：' . $data['ttl'];
+        }
         if (!empty($data['authoritative_dns'])) {
-            $list[] = '权威DNS：' . implode('、', $data['authoritative_dns']);
+            $list[] = '------------------';
+            $list[] = '权威 DNS：' . implode('、', $data['authoritative_dns']);
         }
         if (!empty($data['authoritative_dns_resolve'])) {
-            $list[] = '权威DNS解析：';
+            $list[] = '权威 DNS 解析记录：';
             foreach ($data['authoritative_dns_resolve'] as $item) {
                 $list[] = "    {$item['type']}/{$item['ttl']}：" . implode('、', $item['value']);
             }
         }
+        if (!empty($data['recursive_status'])) {
+            $list[] = '------------------';
+            $list[] = '递归解析状态：' . $data['recursive_status'];
+        }
         if (!empty($data['recursive_dns_resolve'])) {
-            $list[] = '递归DNS解析：';
+            $list[] = '递归 DNS 解析记录：';
             foreach ($data['recursive_dns_resolve'] as $item) {
                 $list[] = "    {$item['type']}/{$item['ttl']}：" . implode('、', $item['value']);
             }
-        }
-        if (!empty($data['ttl'])) {
-            $list[] = 'TTL：' . $data['ttl'];
-        }
-        if (!empty($data['icp_black'])) {
-            $list[] = 'ICP 黑名单：' . ($data['icp_black'] ? '是' : '否');
-        }
-        if (!empty($data['icp_number'])) {
-            $list[] = 'ICP 备案号：' . $data['icp_number'];
-        }
-        if (!empty($data['miit_black'])) {
-            $list[] = 'MIIT 黑名单：' . ($data['miit_black'] ? '是' : '否');
-        }
-        if (!empty($data['http_status'])) {
-            $list[] = 'HTTP 状态：' . $data['http_status'];
-        }
-        if (!empty($data['ping_status'])) {
-            $list[] = 'PING 状态：' . $data['ping_status'];
         }
 
         return implode("\n", $list);
