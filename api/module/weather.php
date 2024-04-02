48b0150f0cb47edea6aa3a617886c1756911a028
若海 <wang@rehiy.com>
2024-04-02 16:34:18 +0800

    fix: 更正风速单位

--- api/module/weather.php
+++ api/module/weather.php
@@ -31,7 +31,7 @@ class Weather
         return implode('', [
             implode('', $data['address']) . "：{$data['description']}；{$data['forecast']}；",
             "气温：{$data['realtime']['temperature']} ℃，体感：{$data['realtime']['apparent_temperature']} ℃；",
-            "相对湿度：" . intval($data['realtime']['humidity'] * 100) . "%；风速：{$data['realtime']['wind']['speed']}米/秒；",
+            "相对湿度：" . intval($data['realtime']['humidity'] * 100) . "%；风速：{$data['realtime']['wind']['speed']}公里/小时；",
             "空气质量：{$data['realtime']['air_quality']['description']['chn']}，PM2.5：{$data['realtime']['air_quality']['pm25']}，PM10：{$data['realtime']['air_quality']['pm10']}；",
             "舒适度指数：{$data['realtime']['life_index']['comfort']['index']}，{$data['realtime']['life_index']['comfort']['desc']}；",
             "紫外线指数：{$data['realtime']['life_index']['ultraviolet']['index']}，{$data['realtime']['life_index']['ultraviolet']['desc']}；",
