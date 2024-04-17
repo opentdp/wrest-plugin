5e79a87ae3b9b9f6f33443f68781d3e9a63fede3
520yxl <1797768493@qq.com>
2024-04-17 22:31:37 +0800

    更换快递查询接口，无需ApiKey，支持顺丰快递查询
    更换快递查询接口，无需ApiKey，支持顺丰快递查询

--- api/module/kuaidi.php
+++ api/module/kuaidi.php
@@ -5,38 +5,49 @@ class Kuaidi
     public static function help()
     {
         return [
-            'YT7458074012883' => '获取快递物流信息,暂不支持顺丰快递',
+            'YT7458074012883' => '获取快递物流信息,已支持顺丰快递查询(SF12312123 1234)',
         ];
     }
 
     public static function simple($args)
     {
         if (empty($args)) {
-            return '参数错误';
-        }
-        if (LINHUN_KEY == '') {
-            return '密钥错误';
-        }
-
-        $data = fetch('https://api.linhun.vip/api/express?apiKey=' . LINHUN_KEY . '&nu=' . $args);
-
-        if ($data['code'] == 200) {
-            $list = [];
-            $list[] =  '快递公司：' . $data['jiancheng'];
-            $list[] = '快递状态：' . $data['kdcode'];
-            $list[] = '快递下单时间：' . $data['fstime'];
-            $arrayLength = count($data['wuliu']);
-            for ($i = 0; $i < $arrayLength; $i++) {
-                $list[] = '物流更新时间：' . $data['wuliu'][$i]['gtime'];
-                $list[] = '物流更新内容：' . $data['wuliu'][$i]['gxbody'];
+            if (IS_WREST) {
+                return '参数错误';
             }
-            return implode("\n", $list);
+            $args = $_SERVER['REMOTE_ADDR'];
         }
-
-        if ($data['code'] != 200) {
-            $list = [];
-            $list[] = $data['msg'];
-            return implode("\n", $list);
+        
+        if (strpos($args, "SF") !== false) {
+               $args = explode(" ", $args);
+            $data = fetch('https://api.songzixian.com/api/express-tracking?dataSource=nationwide_express&trackingNumber=' . $args[0].'&phoneLast4='.$args[1]);
+            if($data['code'] == 200){
+                $list = [];
+                $list[] = '快递单号：' . $data['data']['trackingNumber'];
+                $list[] = '快递公司：' . $data['data']['courierName'];
+                $list[] = '快递状态：' . $data['data']['deliveryStatus'];
+                $list[] = '更新时间：' . $data['data']['latestUpdateTime'];
+                $list[] = '最新信息：' . $data['data']['trackingDetails'][0]['description'];
+                return implode("\n",$list);
+            }else {
+                $list = [];
+                $list[] = '查询顺丰快递必须要寄/收件人手机号码后四位,格式:顺丰快递单号 手机号码后四位 (SF12312123 1234)';
+                return implode("\n",$list);
+            
+            
+            }
+            
+        }else {
+                $data = fetch('https://api.songzixian.com/api/express-tracking?dataSource=nationwide_express&trackingNumber=' . $args);
+            if($data['code'] == 200){
+                $list = [];
+                $list[] = '快递单号：' . $data['data']['trackingNumber'];
+                $list[] = '快递公司：' . $data['data']['courierName'];
+                $list[] = '快递状态：' . $data['data']['deliveryStatus'];
+                $list[] = '更新时间：' . $data['data']['latestUpdateTime'];
+                $list[] = '最新信息：' . $data['data']['trackingDetails'][0]['description'];
+                return implode("\n",$list);
+            }
         }
     }
 }
